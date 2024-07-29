5cd325b84c74e116c86fc3cd0527a86dcfbb4d5f
Vadim Kovalev <onemilligramsmile@gmail.com>
2024-07-29 18:59:57 +0300

    Added tests in tests.py and updated README.md

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +23,63 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_exceeds_length(self, collector):
+        collector.add_new_book('B' * 41)
+        assert len(collector.get_books_genre()) == 0
+
+    def test_add_new_book_duplicate(self, collector):
+        collector.add_new_book('Книга джунглей')
+        collector.add_new_book('Книга джунглей')
+        assert len(collector.get_books_genre()) == 1
+
+    def test_set_book_genre(self, collector):
+        collector.add_new_book('Звездные войны')
+        collector.set_book_genre('Звездные войны', 'Фантастика')
+        assert collector.get_book_genre('Звездные войны') == 'Фантастика'
+
+    def test_set_book_genre_invalid(self, collector):
+        collector.add_new_book('Геркулес')
+        collector.set_book_genre('Геркулес', 'InvalidGenre')
+        assert collector.get_book_genre('Геркулес') == ''
+
+    def test_get_books_with_specific_genre(self, collector):
+        collector.add_new_book('Автостопом по галактике')
+        collector.add_new_book('Мертвая зона')
+        collector.set_book_genre('Автостопом по галактике', 'Фантастика')
+        collector.set_book_genre('Мертвая зона', 'Ужасы')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Автостопом по галактике']
+
+    def test_get_books_for_children(self, collector):
+        collector.add_new_book('Мечтают ли андроиды об электроовцах?')
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Мечтают ли андроиды об электроовцах?', 'Фантастика')
+        collector.set_book_genre('Дракула', 'Ужасы')
+        assert collector.get_books_for_children() == ['Мечтают ли андроиды об электроовцах?']
+
+    def test_add_book_in_favorites(self, collector):
+        collector.add_new_book('На помощь, Дживс')
+        collector.add_book_in_favorites('На помощь, Дживс')
+        assert 'На помощь, Дживс' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites(self, collector):
+        collector.add_new_book('Илиада')
+        collector.add_book_in_favorites('Илиада')
+        collector.delete_book_from_favorites('Илиада')
+        assert 'Илиада' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books(self, collector):
+        collector.add_new_book('Финансист')
+        collector.add_new_book('Таинственный остров')
+        collector.add_book_in_favorites('Финансист')
+        collector.add_book_in_favorites('Таинственный остров')
+        assert set(collector.get_list_of_favorites_books()) == {'Финансист', 'Таинственный остров'}
+
+    @pytest.mark.parametrize("book_name, expected", [
+        ('B' * 40, True),
+        ('B' * 41, False),
+        ('', False),
+        ('A', True),
+    ])
+    def test_add_new_book_boundary_values(self, collector, book_name, expected):
+        collector.add_new_book(book_name)
+        assert (book_name in collector.get_books_genre()) == expected
